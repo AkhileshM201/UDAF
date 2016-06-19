eb34b6de9b5d1366ec09853fe2b4e81fd6165845
Akhilesh <akhilwithhadoop@gmail.com>
2016-06-19 12:37:07 +0530

    change in complexUDAF.java

---
+++ UDAF/src/main/java/com/hive/UDAF/ComplexUDAF.java
@@ -0,0 +1,13 @@
+package com.hive.UDAF;
+
+/**
+ * Hello world!
+ *
+ */
+public class ComplexUDAF 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! this is change" );
+    }
+}
